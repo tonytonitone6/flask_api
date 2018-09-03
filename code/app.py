82af9ceadc7131eac2ecf3d5388658523e79ec24
mao <tonytonitone6@gmail.com>
2018-09-04 02:08:46 +0800

    add register func

--- code/app.py
+++ code/app.py
@@ -1,7 +1,13 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
+
+
 from security import authenticate, identity
+from user import UserRegister
+
+
+
 import json
 
 app = Flask(__name__)
@@ -65,6 +71,7 @@ class ItemList(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 
 
--- code/user.py
+++ code/user.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask_restful import Resource, reqparse
 
 class User:
     def __init__(self, _id, username, password):
@@ -39,3 +40,24 @@ class User:
 
         connection.close()
         return user
+
+
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', type=str, required=True, help="Can you input username")
+    parser.add_argument('password', type=str, required=True, help="Can you input password")
+
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password']))
+
+        connection.commit()
+        connection.close()
+
+        return {"message": "User created successfully", "success": True}, 201
